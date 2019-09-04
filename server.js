7769fbd7bf0a8129926ce18c2fc5a73580506fa9
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-09-04 10:41:25 -0400

    server is up and running

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+
+const db = require('./database/dbConfig.js');
+const Users = require('./users/users-model.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+module.exports = server;
---
+++ users/users-model.js
@@ -0,0 +1,16 @@
+const db = require('../database/dbConfig.js');
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById
+};
+
+function find() {}
+
+function findBy(filter) {}
+
+function add(user) {}
+
+function findById(id) {}
