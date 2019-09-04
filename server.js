82ef5e7f23faf37f89c1462ed34b9671705c8d9d
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-09-04 11:21:51 -0400

    restriction complete

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-// Creates a user using the information sent inside the body of the request.
-// Hash the password before saving the user to the database.
-server.get('/api/users', (req, res) => {
+// If the user is logged in, respond with an array of all the users contained in the database.
+// If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.
+server.get('/api/users', restrict, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -22,8 +22,8 @@ server.get('/api/users', (req, res) => {
     .catch(err => res.send(err));
 });
 
-// If the user is logged in, respond with an array of all the users contained in the database.
-// If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.
+// Creates a user using the information sent inside the body of the request.
+// Hash the password before saving the user to the database.
 server.post('/api/register', (req, res) => {});
 
 // Use the credentials sent inside the body to authenticate the user.
@@ -36,6 +36,28 @@ server.post('/api/login', (req, res) => {});
 // by /api/restricted/. For instance, /api/restricted/something, /api/restricted/other,
 // and /api/restricted/a should all be protected by the middleware; only logged in users should be able
 // to access these routes.
-function restrict(req, res, next) {}
+function restrict(req, res, next) {
+  // Read the username and password from headers, client side is responsible
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res
+            .status(401)
+            .json({ message: 'Whoa There Cowboy, Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Unexpected Error' });
+      });
+  } else {
+    res.status(400).json({ message: 'No credentials provided, partner' });
+  }
+}
 
 module.exports = server;
